ae86886c87900696e34e5b6aa5c6c10bee19d9f3
김수민 <smkim104@naver.com>
2021-07-15 02:30:16 +0900

    2021 카카오 BLIND 채용

---
+++ kakao/soomin_메뉴리뉴얼.cpp
@@ -0,0 +1,84 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+vector<string> orders;
+vector<int> course;
+vector<string> answer;
+map <string, int> m;
+
+bool compare(pair<string, int> a, pair < string, int> b) {
+	return a.second > b.second;
+}
+//DFS로 모든 조합 찾아서 map에 조합별로 개수 넣기
+void dfs(string order, string s, int idx, int count) {
+	if (count == s.length()) {
+		m[s] += 1;
+		return;
+	}
+	for (int i = idx; i < order.length();i++) {
+		s[count] = order[i];
+		dfs(order, s, i + 1, count + 1);
+	}
+
+}
+vector<string> solution(vector<string> orders, vector<int> course) {
+	vector <pair<string, int>> sorted; 
+	for (int i = 0; i < orders.size();i++) {
+		sort(orders[i].begin(), orders[i].end());
+		for (int j = 0; j < course.size();j++) {
+			string s = "";
+			s.resize(course[j]); //참고
+			dfs(orders[i], s, 0, 0);
+		}
+
+	}
+	//많이 시킨 조합의 VALUE 기준으로 정렬
+	for (auto& order : m) {
+		if (order.second <= 1) { //2명 미만 시킨 조합 패스
+			continue;
+		}
+		sorted.push_back(make_pair(order.first, order.second));
+	}
+	sort(sorted.begin(), sorted.end(), compare);
+
+	//각 코스별로 최대값 넣기
+	for (int i = 0; i < course.size();i++) {
+		int max = 0;
+		for (int j = 0; j < sorted.size();j++) {
+			if (sorted[j].first.length() != course[i]) {
+				continue;
+			}
+			else if (max == 0) { //처음 최대값
+				answer.push_back(sorted[j].first);
+				max = sorted[j].second;
+			}
+			else if (max == sorted[j].second) { //최대값 여러개일때
+				answer.push_back(sorted[j].first);
+			}
+			else {
+				break;
+			}
+		}
+	}
+	sort(answer.begin(), answer.end());
+	return answer;
+}
+int main()
+{
+	orders.push_back("ABCFG");
+	orders.push_back("AC");
+	orders.push_back("CDE");
+	orders.push_back("ACDE");
+	orders.push_back("BCFG");
+	orders.push_back("ACDEH");
+	course.push_back(2);
+	course.push_back(3);
+	course.push_back(4);
+
+	solution(orders, course);
+	cout << "Hello World!\n";
+}
+
---
+++ kakao/soomin_순위검색.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+using namespace std;
+vector<string> info;
+vector<string> query;
+map <int, vector<string>> m;
+vector<int> solution(vector<string> info, vector<string> query) {
+	vector<int> answer;
+	vector<string>check;
+	for (int i = 0; i < info.size();i++) {
+		istringstream ss(info[i]);
+		string stringBuffer;
+		while (getline(ss, stringBuffer, ' ')) {
+			m[i].push_back(stringBuffer);
+		}
+		
+	}
+
+	for (int i = 0; i < query.size();i++) {
+		int cnt=0;
+		check.clear();
+		istringstream ss(query[i]);
+		string stringBuffer;
+		while (getline(ss, stringBuffer, ' ')) {
+			if (stringBuffer != "and") {
+				check.push_back(stringBuffer);
+			}	
+		}
+		for (auto& c : m) {
+			bool flag = true;
+			for (int j = 0; j < 4; j++) {
+				if (check[j] == "-") {
+					continue;
+				}
+				else if (c.second[j] != check[j]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == true) {
+				int score = stoi(check[4]);
+				int myscore = stoi(c.second[4]);
+				if (myscore >= score) {
+					cnt += 1;
+				}
+			}
+		}
+		answer.push_back(cnt);
+
+
+	}
+	return answer;
+}
+
+int main() {
+	info.push_back("java backend junior pizza 150");
+	info.push_back("python frontend senior chicken 210");
+	info.push_back("python frontend senior chicken 150");
+	info.push_back("cpp backend senior pizza 260");
+	info.push_back("java backend junior chicken 80");
+	info.push_back("python backend senior chicken 50");
+	query.push_back("java and backend and junior and pizza 100");
+	query.push_back("python and frontend and senior and chicken 200");
+	query.push_back("cpp and - and senior and pizza 250");
+	query.push_back("- and backend and senior and - 150");
+	query.push_back("- and - and - and chicken 100");
+	query.push_back("- and - and - and - 150");
+	solution(info, query);
+}
