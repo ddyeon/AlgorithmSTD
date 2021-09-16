4c2628d6d6aba0ed3299e130ea27e2fb8b389016
ddyeon <53086454+ddyeon@users.noreply.github.com>
2021-09-16 20:01:03 +0900

    Update dayeon_이중우선순위큐.cpp

--- programmers/dayeon_이중우선순위큐.cpp
+++ programmers/dayeon_이중우선순위큐.cpp
@@ -1,66 +1,47 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#include <sstream>
 #include <iostream>
 using namespace std;
-//스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 
-bool cmp(pair<double,int> a, pair<double,int> b)
-{
-    if(a.first == b.first)
-        return a.second < b.second;
-    else
-        return a.first > b.first;
-    
-}
-
-
-vector<int> solution(int N, vector<int> stages)
-{
+vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    vector<pair<double,int>> v; //실패율, 인덱스
+    int cnt=0;
 
-    int cnt = 0;
-    int total_size = stages.size();
-    for(int i = 1; i <=N; i++)
+    priority_queue<int> pq; 
+    priority_queue<int, vector<int>, greater<int>> pq2; 
+    char c; int n;
+    for(int i=0;i<operations.size();i++)
     {
-        total_size = total_size - cnt;
-        cnt = 0;
-        for(int j = 0; j < stages.size(); j++)
+        stringstream ss(operations[i]);
+        ss >> c;
+        ss >> n;
+        if(cnt == 0)
         {
-            if(stages[j] == i)
-                cnt++;
+            while(!pq.empty()) pq.pop();
+            while(!pq2.empty()) pq2.pop();
         }
-        double failure = 0;
-        
-        if(total_size < cnt)
-            failure = 0;
-        else if(cnt == 0)
-            failure = 0;
-        else
-            failure = (double) cnt / total_size;
-
-        //cout << failure << endl;
-        v.push_back({failure, i});
+        if(c == 'I'){
+            pq.push(n);
+            pq2.push(n);
+            cnt++;
+        }else{
+            if(n == 1 and cnt != 0){
+                pq.pop();
+                cnt--;
+            }else if(n == -1 and cnt != 0){
+                pq2.pop();
+                cnt--;
+            }
+        }
     }
-
-    sort(v.begin(), v.end(), cmp);
-
-    for(int i = 0; i < v.size(); i++)
-    {
-        answer.push_back(v[i].second);
-        
+    if(cnt == 0) {
+        answer.push_back(0);
+        answer.push_back(0);
+    }else{
+        answer.push_back(pq.top());
+        answer.push_back(pq2.top());
     }
-
-
-
     return answer;
-
-
-}
-
-int main()
-{   //N=8 , stages=[1,2,3,4,5,6,7]
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7};
-    solution(8, v);
 }
