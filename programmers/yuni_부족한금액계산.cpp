7b939f2af18d70e4571ca031c556689efc8f7c60
Yangyunhui454545 <43237961+Yangyunhui454545@users.noreply.github.com>
2021-10-04 15:53:46 +0900

    Create yuni_부족한금액계산.cpp
    위클리1주차

---
+++ programmers/yuni_부족한금액계산.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+    int index = 1;
+    long long mon= money;
+    while(count-- > 0){
+        mon -= price * index;
+        index++;
+    }
+    if(mon >= 0)answer = 0;
+    else answer = -mon;
+    return answer;
+}
